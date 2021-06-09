6afa8828f47f2d362ed329612a09050e824442b8
nabil053 <nabil.cse.53@gmail.com>
2021-06-09 23:30:01 +0600

    Divided data into training, cross-validation and testing data

--- Main.py
+++ Main.py
@@ -7,6 +7,15 @@ np.set_printoptions(suppress=True)
 
 df = pd.read_csv('water_potability.csv')
 df = df.dropna().reset_index(drop=True)
-#df = df.sample(frac=1)
-print(df)
-#df.to_csv('water_potability.csv')
+#print(df)
+
+size_training = (3 * df.shape[0])//5
+size_validation = df.shape[0]//5
+size_testing = df.shape[0] - (size_training + size_validation)
+#print(size_training, size_validation, size_testing)
+
+df_train = (df.iloc[0:size_training,:]).reset_index(drop=True)
+df_cv = (df.iloc[size_training:(size_training + size_validation),:]).reset_index(drop=True)
+df_test = (df.iloc[(size_training + size_validation):,:]).reset_index(drop=True)
+print(df_train)
+
