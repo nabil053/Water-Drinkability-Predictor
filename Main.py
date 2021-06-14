8339802927214546f36f421227fe168e5362b49c
nabil053 <nabil.cse.53@gmail.com>
2021-06-15 00:47:22 +0600

    Modified model to use tuning parameter in determining best model parameters, and testing each model against cross validation set to determine the best tuning parameter and model parameters

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import math
+import sys
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,92 +24,102 @@ df_cv = (df.iloc[size_training:(size_training + size_validation),:]).reset_index
 df_test = (df.iloc[(size_training + size_validation):,:]).reset_index(drop=True)
 #print(df_train.columns)
 
-#Initializing parameters vector to 0 and copying features of training data to a matrix with zero feature included
-w = np.zeros(df_train.shape[1])
+#Copying features of training, cross validation and testing data to respective matrices with zero feature included
 matrix_train = np.ones((df_train.shape[0], df_train.shape[1]))
 matrix_train[:,1:] = df_train.iloc[:,:-1]
+matrix_cv = np.ones((df_cv.shape[0], df_cv.shape[1]))
+matrix_cv[:,1:] = df_cv.iloc[:,:-1]
+matrix_test = np.ones((df_test.shape[0], df_test.shape[1]))
+matrix_test[:,1:] = df_test.iloc[:,:-1]
+# matrix_cv = np.ones((df_cv.shape[0], df_cv.shape[1]))
+# matrix_cv[:,1:] = df_cv.iloc[:,:-1]
 
-#Normalizing the data in the matrix
+#Normalizing the data in the matrices
 for i in range(1,matrix_train.shape[1]):
     mean = np.mean(matrix_train[:,i])
     std = np.std(matrix_train[:,i])
     matrix_train[:,i] = (matrix_train[:,i] - mean)/std
+for i in range(1,matrix_cv.shape[1]):
+    mean = np.mean(matrix_cv[:,i])
+    std = np.std(matrix_cv[:,i])
+    matrix_cv[:,i] = (matrix_cv[:,i] - mean)/std
+for i in range(1,matrix_test.shape[1]):
+    mean = np.mean(matrix_test[:,i])
+    std = np.std(matrix_test[:,i])
+    matrix_test[:,i] = (matrix_test[:,i] - mean)/std
 #print(matrix_train)
 
-#Initializing step size and stopping criteria
+#Initializing parameter vector to 0 as well as starting step size and stopping criteria
 alpha = 0.01
-stepsize = alpha
 stop_criteria = 0.001
+w = np.zeros(df_train.shape[1])
 #print(matrix_train)
 
-#Iteration for determining parameters using gradient ascent
-i = 1
-while(True):
-    #Intermediate calculations of determining partial derivative of maximum likelihood
-    temp = np.apply_along_axis(lambda x: np.matmul(x, w), 1, matrix_train)
-    for j in range(len(temp)):
-        if temp[j] > 10:
-            temp[j] = 1
-        elif temp[j] < -10:
-            temp[j] = 0
+#Selecting some possible tuning parameter values and the variables needed to find the best one
+param_vals = [0, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+tuning_param = 0
+min_error = df_cv.shape[0]
+error = 0
+
+#Carrying out gradient ascent for each possible value of tuning parameter
+for p in param_vals:
+    i = 1
+    stepsize = alpha
+    w_temp = np.zeros(df_train.shape[1])
+
+    # Gradient ascent iteration for determining parameters using gradient ascent
+    while(True):
+        #Intermediate calculations of determining partial derivative of maximum likelihood
+        temp = np.apply_along_axis(lambda x: np.matmul(x, w_temp), 1, matrix_train)
+        for j in range(len(temp)):
+            if temp[j] > 10:
+                temp[j] = 1
+            elif temp[j] < -10:
+                temp[j] = 0
+            else:
+                temp[j] = 1 / (1 + math.exp(temp[j] * (-1)))
+        temp = df_train.iloc[:, -1:].to_numpy().flatten() - temp
+        temp = np.apply_along_axis(lambda x: np.multiply(x, temp), 0, matrix_train)
+
+        #Final result of maximum likelihood's partial derivative vector, as well as its magnitude
+        partial = np.apply_along_axis(lambda x: np.sum(x), 0, temp)
+        partial_sqrt = np.linalg.norm(partial)
+
+        #Updating the parameters vector
+        w_temp = ((1 - (2 * p * stepsize)) * w_temp) + (stepsize * partial)
+
+        #Updating the stepsize
+        stepsize = alpha / (i + 1)
+
+        #If not the first step and the change in magnitude of partial derivative is below criteria value, end iteration
+        if i != 1 and math.fabs(partial_sqrt - partial_sqrt_prev) <= stop_criteria:
+            break
+
+        partial_sqrt_prev = partial_sqrt
+        i = i + 1
+
+    #Checking the error of current model's performance in cross validation data
+    temp = np.apply_along_axis(lambda x: np.matmul(x, w_temp), 1, matrix_cv)
+    for k in range(len(temp)):
+        if temp[k] > 10:
+            temp[k] = 1
+        elif temp[k] < -10:
+            temp[k] = 0
         else:
-            temp[j] = 1 / (1 + math.exp(temp[j] * (-1)))
-    temp = df_train.iloc[:, -1:].to_numpy().flatten() - temp
-    temp = np.apply_along_axis(lambda x: np.multiply(x, temp), 0, matrix_train)
+            temp[k] = 1 / (1 + math.exp(temp[k] * (-1)))
+    temp = df_cv.iloc[:, -1:].to_numpy().flatten() - temp
+    error = (np.absolute(temp)).sum()
 
-    #Final result of maximum likelihood's partial derivative vector, as well as its magnitude
-    partial = np.apply_along_axis(lambda x: np.sum(x), 0, temp)
-    partial_sqrt = np.linalg.norm(partial)
+    #Updating the tuning parameter and model parameters if error is less than in previous iteration
+    if error < min_error:
+        min_error = error
+        tuning_param = p
+        w = np.copy(w_temp)
 
-    #Updating the parameters vector
-    w = w + (stepsize * partial)
 
-    #Updating the stepsize
-    stepsize = alpha / (i + 1)
-
-    #If not the first step and the change in magnitude of partial derivative is below criteria value, end iteration
-    if i != 1 and math.fabs(partial_sqrt - partial_sqrt_prev) <= stop_criteria:
-        break
-
-    partial_sqrt_prev = partial_sqrt
-    i = i + 1
-
-print(i, w)
-true_zero = 0
-false_zero = 0
-true_one = 0
-false_one = 0
-is_zero = False
-is_one = False
-for i in range(df_train.shape[0]):
-    #d = df_train.iloc[i,:-1].to_numpy()
-    d = matrix_train[i,:]
-    #d = np.insert(d, 0, 1)
-    t = np.matmul(d,w)
-    if t > 10:
-        is_one = True
-        is_zero = False
-    elif t < -10:
-        is_one = False
-        is_zero = True
-    else:
-        tp = 1 / (1 + math.exp((-1) * t))
-        if tp > 0.5:
-            is_one = True
-            is_zero = False
-        else:
-            is_one = False
-            is_zero = True
-    if (df_train.iloc[i,-1] == 0) and is_zero:
-        true_zero = true_zero + 1
-    elif (df_train.iloc[i,-1] == 1) and is_zero:
-        false_zero = false_zero + 1
-    elif (df_train.iloc[i,-1] == 1) and is_one:
-        true_one = true_one + 1
-    elif (df_train.iloc[i,-1] == 0) and is_one:
-        false_one = false_one + 1
 #print(math.exp(np.matmul(d,w)))
 # print(true_zero)
 # print(false_zero)
 # print(true_one)
 # print(false_one)
+# print(((true_zero + true_one)/df_cv.shape[0])*100)
